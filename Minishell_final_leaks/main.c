369d629dd1e6b47d8817170e4bb0499cfb43da1f
Nuno Taboada <nunotaboada@gmail.com>
2023-07-04 09:29:18 +0100

    home

---
+++ Minishell_final_leaks/main.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsoares- <nsoares-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 09:12:57 by nsoares-          #+#    #+#             */
+/*   Updated: 2023/05/09 13:22:50 by nsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	g_ex_status;
+
+int	is_operator(char c)
+{
+	if (c == 60)
+		return (1);
+	if (c == 62)
+		return (1);
+	if (c == 124)
+		return (1);
+	return (0);
+}
+
+void	ft_wait(t_shell *sh)
+{
+	int			wstatus;
+
+	if (waitpid(sh->pid, &wstatus, 0) != -1)
+	{
+		if (WIFSIGNALED(wstatus))
+			wstatus += 128;
+		if (sh->fork)
+			g_ex_status = wstatus;
+		if (g_ex_status == 130)
+			ft_putchar_fd('\n', STDOUT_FILENO);
+		sh->proc--;
+	}
+	while (sh->proc)
+	{
+		wait(0);
+		sh->proc--;
+	}
+	if (g_ex_status > 255)
+		g_ex_status = g_ex_status / 255;
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_shell		shell;
+
+	init(ac, av, envp, &shell);
+	while (1)
+	{
+		signals();
+		shell.cmd_line = show_prompt(&shell);
+		if (!shell.cmd_line)
+			continue ;
+		add_history(shell.cmd_line);
+		if (!words(&shell))
+			execcmd(&shell);
+		ft_wait(&shell);
+		// mtr_free(shell.envp);
+		free(shell.cmd_line);
+		free_listtoken(shell.head_token);
+		free_listcmd(shell.cmds);
+		shell.head_token = NULL;
+		shell.cmds = NULL;
+	}
+	free_all(&shell);
+	return (0);
+}
---
+++ Minishell_final_leaks/src/utils/utils.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsoares- <nsoares-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 20:55:12 by nsoares-          #+#    #+#             */
+/*   Updated: 2023/05/17 10:22:35 by nsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+int	nb_of_args(t_cmds *cmds)
+{
+	int		i;
+
+	i = 0;
+	while (cmds->cmd_line[i] != NULL)
+		i++;
+	return (i);
+}
+
+int	pos_char(char *str, char c)
+{
+	int		i;
+
+	if (!str)
+		return (-1);
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == c)
+			return (i);
+	}
+	if (c == '\0')
+		return (i);
+	return (-1);
+}
+
+void	put_var_env_from_cmd(char *cmd, t_shell *shell)
+{
+	char	*var;
+	char	*value;
+	char	*equals;
+
+	equals = ft_strchr(cmd, '=');
+	if (equals)
+	{
+		var = ft_substr(cmd, 0, equals - cmd);
+		value = ft_strdup(equals + 1);
+	}
+	else
+	{
+		var = ft_strdup(cmd);
+		value = NULL;
+	}
+	put_var_env(var, value, shell);
+	free(var);
+	free(value);
+}
