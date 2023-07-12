c4e55bb9dd2193a91242c8258d5a22a0dce035f6
Nuno Taboada <nunotaboada@gmail.com>
2023-07-12 13:26:15 +0100

    home

---
+++ Minishell_final_leaks_5/src/builtins/exit.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmoreira <nmoreira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 13:24:09 by nsoares-          #+#    #+#             */
+/*   Updated: 2023/07/10 18:21:55 by nmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+int	args_numbers(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '-' || str[i] == '+')
+			i++;
+		if (!ft_isdigit(str[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	built_exit(t_cmds *cmds)
+{
+	int	nb_args;
+	int	exit_status;
+
+	nb_args = nb_of_args(cmds);
+	if (nb_args > 1 && args_numbers(cmds->cmd_line[1]) == 1)
+	{
+		printf("exit\n");
+		printf("bash: exit: %s: numeric argument required\n", cmds->cmd_line[1]);
+		exit_status = 2;
+		exit(g_ex_status = exit_status);
+	}
+	if (nb_args == 2 && args_numbers(cmds->cmd_line[1]) == 0)
+		g_ex_status = ft_atoi(cmds->cmd_line[1]);
+	printf("exit\n");
+	if (nb_args > 2 && args_numbers(cmds->cmd_line[1]) == 0)
+	{
+		printf("bash: exit: too many arguments\n");
+		exit_status = 1;
+		return (g_ex_status = exit_status);
+	}
+	exit(g_ex_status);
+}
---
+++ Minishell_final_leaks_5/src/envp.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   envp.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmoreira <nmoreira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 19:09:38 by nmoreira          #+#    #+#             */
+/*   Updated: 2023/07/11 19:18:06 by nmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+/*alterado no if tinha var em vez de envp[i]*/
+
+int	pos_envp(char *var, char **envp)
+{
+	int		i;
+	int		len;
+
+	if (!envp || !*envp)
+		return (-1);
+	len = ft_strlen(var);
+	i = -1;
+	while (envp[++i])
+	{
+		if (!ft_strncmp(envp[i], var, len))
+		{
+			if (pos_char(envp[i], '=') == len || pos_char(envp[i], '\0') == len)
+				return (i);
+		}
+	}
+	return (-1);
+}
+
+char	*get_env(char *var, t_shell *sh)
+{
+	int		pos;
+
+	pos = pos_envp(var, sh->envp);
+	if (pos < 0 || !ft_strchr(sh->envp[pos], '='))
+		return (NULL);
+	return (ft_strdup(sh->envp[pos] + (ft_strlen(var) + 1)));
+}
+
+int	put_var_env(char *var, char *value, t_shell *shell)
+{
+	int		pos;
+	char	*aux;
+	char	*aux1;
+
+	aux = ft_strjoin(var, "=");
+	aux1 = ft_strjoin(aux, value);
+	free(aux);
+	pos = pos_envp(var, shell->envp);
+	if (value == NULL)
+	{
+		if (pos < 0)
+			shell->envp = mtr_addnew(var, shell->envp);
+		return (0);
+	}
+	if (pos < 0)
+	{
+		shell->envp = mtr_addnew(aux1, shell->envp);
+		free(aux1);
+		return (0);
+	}
+	aux = shell->envp[pos];
+	shell->envp[pos] = aux1;
+	free(aux);
+	return (0);
+}
+
+void	remv_var_env(char *var, t_shell *shell)
+{
+	int		pos;
+
+	pos = pos_envp(var, shell->envp);
+	if (pos < 0)
+		return ;
+	shell->envp = mtr_rmv(pos, shell->envp);
+}
